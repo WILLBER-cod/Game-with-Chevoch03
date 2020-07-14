8f153547ff4d98231545e436fa3fbb1c07b69d95
Vladimir <vovazadorozhin14@gmail.com>
2020-07-14 11:13:44 +0800

    lol

---
+++ Как получить люлей или гта 10/Assets/Character/Anim.cs
@@ -0,0 +1,27 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class Anim : MonoBehaviour
+{
+    Animator anim;
+
+    void Start()
+    {
+        anim = GetComponent<Animator>();
+    }
+
+    void Update()
+    {
+        if (Input.GetKey(KeyCode.W) || Input.GetKey(KeyCode.A) || Input.GetKey(KeyCode.S) || Input.GetKey(KeyCode.D))
+        {
+            anim.SetBool("move", true);
+        }
+        else
+        {
+            anim.SetBool("move", false);
+        }
+       
+    }
+
+}
